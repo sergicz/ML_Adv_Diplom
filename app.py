e356294edd9d76ad0abb4cd55699d244644aa93a
sergicz <sergicz@yandex.ru>
2026-09-13 23:13:42 +0500

    1

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import requests      # Библиотека для отправки запрос
 import datetime      # Библиотека для даты
 
 app = Flask(__name__)
+app.json.ensure_ascii = False
 
 # Получаем настройки из переменных окружения
 ch_host = os.getenv('CLICKHOUSE_HOST', 'localhost')
@@ -54,7 +55,7 @@ def imp_calls():
         iNext = cNext.result_rows[0][0] #инициализируем счетчик страниц
         iLastID = iNext
         d = dict() #инициализация словаря для накопления данных
-        while iNext>=0 and iNext < 100: #ограничим для теста количество записей, в реале 500К записей грузятся 5 часов
+        while iNext>=0 and iNext < 1000: #ограничим для теста количество записей, в реале 500К записей грузятся 5 часов
             response = requests.get(page_link+str(iNext), headers={'User-Agent': UserAgent().chrome}) #получаем порцию данных из Б24
             profile = json.loads(response.content.decode('utf-8')) #запрос возвращает 50 записей за раз
             cRes=profile['result'] #потрошим результат запроса
@@ -64,14 +65,15 @@ def imp_calls():
                 iNext=-999
             for cEl in cRes: #перебираем текущие 50 записей
                 cDate = cEl['CALL_START_DATE'][:10] #выкусываем дату из строки
-                if cEl['PORTAL_USER_ID'] in ('16','17','3062','3068','144'): #фильтруем звонки по консультантам техподдержки
-                    if cDate not in d: #если такой даты еще нет в словаре - добавляем, зануляем счетчик звонков и сразу присваиваем сумму из csv
+                if cEl['PORTAL_USER_ID'] in ('16','17','3062','3068','144','166'): #фильтруем звонки по консультантам техподдержки
+                    if cDate not in d: #если такой даты еще нет в словаре - добавляем, зануляем счетчик звонков 
                         d[cDate] = 0
                     d[cDate] += 1 #плюсуем счетчик звонков
                 iLastID+=1    
             print('Читаем следующую партию: '+str(iNext) +  ' Дата: '+ cDate)
         for el in d: #перекидываем данные из словаря в CH
-            client.query(f'INSERT INTO itex.b24 (date, calls) VALUES ({el}, {d[el]})')
+            print(f'Записываем: {el}, {d[el]}')
+            client.query(f'INSERT INTO itex.b24 (dat, calls) VALUES ({el}, {d[el]})')
             client.query('alter table itex.next delete where 1=1')
             client.query(f'insert INTO itex.next (next) VALUES ({iLastID})')
         return jsonify({
