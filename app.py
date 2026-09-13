db3d58a258b75b4817783a96e9ec087728ec6fc8
sergicz <sergicz@yandex.ru>
2026-09-13 23:28:07 +0500

    1

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def imp_calls():
             print('Читаем следующую партию: '+str(iNext) +  ' Дата: '+ cDate)
         for el in d: #перекидываем данные из словаря в CH
             print(f'Записываем: {el}, {d[el]}')
-            client.query(f'INSERT INTO itex.b24 (dat, calls) VALUES ({el}, {d[el]})')
+            client.query(f"INSERT INTO itex.b24 (dat, calls) VALUES ('{el}', {d[el]})")
             client.query('alter table itex.next delete where 1=1')
             client.query(f'insert INTO itex.next (next) VALUES ({iLastID})')
         return jsonify({
