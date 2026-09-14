7afc569ede29e249739753052d2dd85f1e93089c
sergicz <sergicz@yandex.ru>
2026-09-15 00:57:00 +0500

    1

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ import json
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-logger = logging.getLogger(__name__)
 app = Flask(__name__)
 app.json.ensure_ascii = False
 
